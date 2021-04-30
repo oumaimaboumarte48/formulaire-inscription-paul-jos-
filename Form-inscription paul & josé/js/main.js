10f6af735f9794c8e3be5315b8e4074b09420fb1
oumaimaboumarte48 <57942794+oumaimaboumarte48@users.noreply.github.com>
2021-04-30 08:44:49 +0000

    Add files via upload

---
+++ Form-inscription paul & josé/js/main.js
@@ -0,0 +1,63 @@
+//eye function change type of input from text to password or the opposite
+//receive the id of the input
+function eye(id) {
+  if (document.getElementById(id).type == "text") {
+    //if the type is text change it to password
+    return (document.getElementById(id).type = "password");
+  }
+  //if the type is password change it to text
+  return (document.getElementById(id).type = "text");
+}
+
+//Email validation check if the email is vad
+function validateEmail(email) {
+  //regular expression
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //check if email is valid and turn it to lowerCase
+  if (re.test(String(email).toLowerCase()) == false) {
+    return alert("Please enter a valid email ");
+  }
+  return true;
+}
+//Password Validation Check if the password is valid
+function validatePassword(password, passwordConfirmaion) {
+  console.log("nam");
+  if (password !== passwordConfirmaion) {
+    //Password Validation Check if the password is the same as passwordConfirmaion
+    return alert("Password does not match");
+  }
+  if (password.length >= 5) {
+    //check if password has more than 4 chars
+    return alert("Password Empty");
+  }
+  return true;
+}
+//check if name is valid
+function validateName(name) {
+  //check if name have more than 3 chars
+  if (name.length <= 3) {
+    return alert("Please enter a valid name");
+  }
+  return true;
+}
+
+//validate all form
+function validate() {
+  //get Email value
+  const email = document.getElementById("email").value;
+  //get password value
+  const password = document.getElementById("password").value;
+  //get name value
+  const nom = document.getElementById("nom").value;
+  //get password Confirmation value
+  const passwordConfirmaion = document.getElementById("eyeConfirmation").value;
+  //validate using fuctions
+  if (
+    validateEmail(email) &&
+    validatePassword(password, passwordConfirmaion) &&
+    validateName(nom)
+  ) {
+    alert("Form is Valid");
+  }
+  return false;
+}
